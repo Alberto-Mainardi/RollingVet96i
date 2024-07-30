8dd4785d0823a87785be671e88ac6bbc89e62252
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-07-30 19:46:37 -0300

    Se realizo el estilo de la vista. Se agrego color de fondo, color de texto y color de fondo de las Cards.

--- rollingVet/front/src/components/AvatarCard.jsx
+++ rollingVet/front/src/components/AvatarCard.jsx
@@ -4,11 +4,12 @@ import CardBody from 'react-bootstrap/esm/CardBody';
 import 'bootstrap/dist/css/bootstrap.min.css'
 const AvatarCard = (props) => {
   return (
-    <Card className='mt-4 mb-2'>
-        <Card.Img  variant='top' src='https://img.freepik.com/psd-gratis/ilustracion-3d-avatar-o-perfil-humano_23-2150671142.jpg?size=338&ext=jpg'/>
-        <Card.Body>
-            <Card.Title>{props.nombre}</Card.Title>
-            <Card.Text>{props.descripcion}</Card.Text>
+    <Card className='mt-4 mb-2 mx-md-4 d-flex align-items-center rounded-4'>
+        <Card.Body className='bg-Card rounded-4'>
+        <Card.Title className='my-2'>{props.nombre}</Card.Title>
+        <Card.Img  className='w-75 rounded-4' variant='top' src='https://img.freepik.com/psd-gratis/ilustracion-3d-avatar-o-perfil-humano_23-2150671142.jpg?size=338&ext=jpg'/>
+        
+            <Card.Text className='mt-4'>{props.descripcion}</Card.Text>
         </Card.Body>
     </Card>
   )
--- rollingVet/front/src/views/Nosotros.jsx
+++ rollingVet/front/src/views/Nosotros.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import AvatarCard from '../components/AvatarCard'
 import CardBody from 'react-bootstrap/esm/CardBody';
-
+import '../App.css'
 const Nosotros = () => {
 
-  
+
 
   let devs = [
     {
@@ -16,27 +16,30 @@ const Nosotros = () => {
       descripcion: "Desarrollador FullStack"
     },
     {
-      nombre: "Felipe Sarco",
+      nombre: "Felipe Raúl Sarco",
       descripcion: "Desarrollador FullStack"
     }
   ];
 
   return (
-    <main className='d-flex flex-column mt-4'>
-      <h1>Acerca de Nosotros</h1>
+    <main className='d-flex flex-column mt-4 bg-color1'>
+      <h1 className='texto-color1'>Acerca de Nosotros</h1>
       <section>
-        <article className='my-4'>
-          
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corrupti dolor consequatur odio vel magnam, pariatur repellendus, nesciunt nisi recusandae alias ad. Deleniti vitae neque eos atque. Saepe, dolorum exercitationem.
+        <article className='m-4'>
+
+          <p className='mx-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corrupti dolor consequatur odio vel magnam, pariatur repellendus, nesciunt nisi recusandae alias ad. Deleniti vitae neque eos atque. Saepe, dolorum exercitationem.
             Nisi minima possimus accusantium recusandae sunt nobis ratione eveniet quos ipsam consequatur ab, vero unde officiis aut saepe consequuntur aspernatur, quisquam officia? Distinctio eligendi dolorem quis animi aperiam laborum perferendis.
           </p>
         </article>
 
         <article className='my-4'>
-          <h2>Integrantes</h2>
-          {devs.map((dev)=>{
-            return <AvatarCard nombre={dev.nombre} descripcion={dev.descripcion}/>
-          })}
+          <h2 className='my-4 texto-color1'>Integrantes</h2>
+          <div className='d-flex flex-column align-items-center justify-content-center flex-md-row'>
+            {devs.map((dev,index) => {
+              return <AvatarCard key={`avatarCard${index}`} nombre={dev.nombre} descripcion={dev.descripcion} />
+            })}
+          </div>
+
         </article>
       </section>
 
