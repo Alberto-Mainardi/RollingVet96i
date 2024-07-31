fbbe9f2021d89f1b2c749894b151ac93b522e62f
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-07-30 21:36:51 -0300

    Se agrego estilo al NavBar. Falta agregrar logo. Se creo la variable css --color-principal y la clase bg-navBar. Se creo una media query para cambiar la altura del NavBar.

--- rollingVet/front/src/common/NavbarApp.jsx
+++ rollingVet/front/src/common/NavbarApp.jsx
@@ -1,55 +1,60 @@
-import { Navbar, Nav } from 'react-bootstrap';
 import { Link, NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import administrarPacienteLogo from '../assets/media/administrarPacienteLogo.svg'
+import administrarTurnoLogo from '../assets/media/administrarTurnosLogo.svg'
 const NavbarApp = () => {
     return (
 
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home"> <Link className='navbar-branch' to="/">
-                    <img src="" alt="logo" />
-                </Link></Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" className=" bg-navBar w-100 navBar-Height align-items-center">
+            <Container className='m-0'>
+                <Link className='navbar-branch' to="/">
+                
+                   
+                    <img  className='w-50' src="" alt="logo" />
+                
+                
+                </Link>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-4">
 
 
-                        <NavLink className="nav-link" to="/">
+                        <NavLink className="nav-link text-white fs-5" to="/">
                             Inicio
                         </NavLink>
 
-                        <NavLink className="nav-link" to="/nosotros">
+                        <NavLink className="nav-link  text-white fs-5" to="/nosotros">
                             Nosotros
                         </NavLink>
-                        <NavLink className="nav-link" to="/planes">
+                        <NavLink className="nav-link  text-white fs-5" to="/planes">
                             Planes
                         </NavLink>
-                        <NavLink className="nav-link" to="/contacto">
+                        <NavLink className="nav-link  text-white fs-5" to="/contacto">
                             Contáctanos
                         </NavLink>
 
 
-                        <NavLink className="nav-link" to="/ingresar">
+                        <NavLink className="nav-link  text-white fs-5" to="/ingresar">
                             Ingresar
                         </NavLink>
-                        <NavLink className="nav-link" to="/registrarse">
+                        <NavLink className="nav-link  text-white fs-5" to="/registrarse">
                             Registrarse
                         </NavLink>
 
-                        <NavLink className="nav-link" to="/admin/gestionPacientes">
-                            Administrar Pacientes
+                        <NavLink className="nav-link  text-white fs-5" to="/admin/gestionPacientes">
+                            <img  className='' src={administrarPacienteLogo} alt="logo" />
                         </NavLink>
-                        <NavLink className="nav-link" to="/admin/gestionTurnos">
-                            Administrar Turnos
+                        <NavLink className="nav-link  text-white fs-5" to="/admin/gestionTurnos">
+                        <img  className='' src={administrarTurnoLogo} alt="logo" />
                         </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+       
     )
 }
 
