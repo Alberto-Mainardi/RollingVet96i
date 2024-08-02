1f9bf1d44daf2797f8cc745a4617cb5ab47c0316
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-08-02 18:21:20 -0300

    Se agrego funcionalidad de mensaje automatico al mail de la empresa con EmailJs. Se finalizo el responsive. Falta agregar detalles en el estilo.

---
+++ rollingVet/front/src/components/FormContacto.jsx
@@ -0,0 +1,104 @@
+import { useState, useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import emailjs from '@emailjs/browser';
+import InputGroup from 'react-bootstrap/InputGroup';
+import userLogo from '../assets/media/userLogo.svg'
+import mensajeLogo from '../assets/media/mensajeLogo.svg'
+
+function FormContacto() {
+    const [validated, setValidated] = useState(false);
+    const formEmail = useRef();
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+        sendEmail(event);
+    };
+
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs
+            .sendForm('service_jwmw855', 'template_ue7m6oa', formEmail.current, {
+                publicKey: '6IexsJMDhf7T84v_8',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
+    return (
+        <Form className=' p-4 rounded-4' ref={formEmail} noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+                <Col>
+                
+                <Form.Group  className='text-start' controlId="validationCustom01">
+                    <Form.Label>Nombre y Apellido</Form.Label>
+                    <InputGroup>
+                    
+                    <InputGroup.Text>
+                        <img src={userLogo} alt="" />
+                    </InputGroup.Text>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Nombre y Apellido"
+                        name="user_name"
+                    />
+                    </InputGroup>
+                </Form.Group>
+                <Form.Group  className='text-start my-4' controlId="validationCustom02">
+                    <Form.Label>Email</Form.Label>
+                <InputGroup>
+                    <InputGroup.Text className='fw-bold fs-5'>
+                        @
+                    </InputGroup.Text>
+                    <Form.Control
+                        required
+                        type="email"
+                        placeholder="Email"
+                        name="reply_to"
+                    />
+                </InputGroup>    
+                </Form.Group>
+                <Form.Group  className='text-start' controlId="validationCustomUsername">
+                    <Form.Label>Mensaje</Form.Label>
+                    <InputGroup>
+                    <InputGroup.Text>
+                        <img src={mensajeLogo} alt="" />
+                    </InputGroup.Text>
+                    <Form.Control
+                        name="message"
+                        as="textarea" rows={5}
+                        aria-describedby="inputGroupPrepend"
+                        required
+                    />
+                    
+                    <Form.Control.Feedback type="invalid">
+                       Por favor ingrese un mensaje.
+                    </Form.Control.Feedback>
+                    </InputGroup>
+                </Form.Group>
+                </Col>
+            </Row>
+
+            <Button type="submit" className=''>Enviar</Button>
+        </Form>
+    );
+}
+
+export default FormContacto;
--- rollingVet/front/src/components/Sucursales.jsx
+++ rollingVet/front/src/components/Sucursales.jsx
@@ -3,33 +3,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import wpLogo from '../assets/media/whatsappLogo.svg'
 import ubicacionLogo from '../assets/media/ubicacionLogo.svg'
 import calendarioLogo from '../assets/media/calendarioLogo.svg'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col';
+
 const Sucursales = (props) => {
     let sucursal = props.sucursal;
     return (
-        <section className='m-4 bg-sucursal rounded-4'>
+        <div className='m-4 bg-sucursal rounded-4'>
             <h2 className='my-4'>Sucursal {sucursal.id}</h2>
             <article id='sucursalContainer'>
                 {sucursal.mapa}
-                <div className='d-sm-flex align-items-center justify-content-around flex-sm-row'>
+                <Container fluid >
 
-                    <div>
-                        <img src={ubicacionLogo} alt="ubicacionLogo" />
-                        <p>{sucursal.direccion}</p>
-                    </div>
+                    <Row>
+                        <Col  className='p-2'> 
+                            <img className='logos-lg' src={ubicacionLogo} alt="ubicacionLogo" />
+                            <p>{sucursal.direccion}</p>
+                        </Col>
 
-                    <div>
-                        <img src={calendarioLogo} alt="calendarioLogo" />
-                        <p className='text-center'>Lunes a Viernes: 9:00 a 20:00 hs <br /> Sabado: 9:00 a 14:00 hs</p>
-                    </div>
+                        <Col lg={{order:'last',span:12}} xl="auto" >
+                            <img className='logos-lg' src={calendarioLogo} alt="calendarioLogo" />
+                            <p className='text-center'>Lunes a Viernes: 9:00 a 20:00 hs <br /> Sabado: 9:00 a 14:00 hs</p>
+                        </Col>
 
-                    <div>
-                        <img src={wpLogo} alt="whatsappLogo" />
-                        <p>{sucursal.telefono}</p>
-                    </div>
+                        <Col  className='p-2'  xxl={{order:'last'}} >
+                            <img className='logos-lg' src={wpLogo} alt="whatsappLogo" />
+                            <p>{sucursal.telefono}</p>
+                        </Col>
+                    </Row>
 
-                </div>
+
+                </Container>
             </article>
-        </section>
+        </div>
     )
 }
 
