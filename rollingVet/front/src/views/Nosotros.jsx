8f0c09b0b1f98fef14c497633cb35674182bdfeb
FelipeSarco17 <sarcofelipe456@gmail.com>
2024-08-03 19:18:56 -0300

    Se agrego imagen de fondo y se cambio la fuente de los titulos.

--- rollingVet/front/src/views/Nosotros.jsx
+++ rollingVet/front/src/views/Nosotros.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import AvatarCard from '../components/AvatarCard'
 import CardBody from 'react-bootstrap/esm/CardBody';
 import '../App.css'
+import '../estilos/nosotros.css'
 const Nosotros = () => {
 
 
@@ -22,18 +23,18 @@ const Nosotros = () => {
   ];
 
   return (
-    <main className='d-flex flex-column mt-4 bg-color1'>
-      <h1 className='texto-color1'>Acerca de Nosotros</h1>
-      <section>
+    <main className='d-flex flex-column bg-color1 bg-nosotros'>
+      <section className='p-4 bg-color1-50'>
+      <h1 className='texto-color1 font-title mt-4'>Acerca de Nosotros</h1>
+      
         <article className='m-4'>
 
-          <p className='mx-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique corrupti dolor consequatur odio vel magnam, pariatur repellendus, nesciunt nisi recusandae alias ad. Deleniti vitae neque eos atque. Saepe, dolorum exercitationem.
-            Nisi minima possimus accusantium recusandae sunt nobis ratione eveniet quos ipsam consequatur ab, vero unde officiis aut saepe consequuntur aspernatur, quisquam officia? Distinctio eligendi dolorem quis animi aperiam laborum perferendis.
+          <p className='mx-4 fs-5 text-center '>Somos el equipo desarrollador de la web de RollingVet, nos centramos en el trabajo en equipo basado en la comunicacion, respeto y organizacion. Juntos esperamos brindarte el mejor servicio para que puedas cuidar a tus mascotas de la mejor manera desde la comodidad de tu casa. Gracias por confiar en nosotros.
           </p>
         </article>
 
         <article className='my-4'>
-          <h2 className='my-4 texto-color1'>Integrantes</h2>
+          <h2 className='my-4 texto-color1 font-title'>Nuestros Profesionales</h2>
           <div className='d-flex flex-column align-items-center justify-content-center flex-md-row'>
             {devs.map((dev,index) => {
               return <AvatarCard key={`avatarCard${index}`} nombre={dev.nombre} descripcion={dev.descripcion} />
