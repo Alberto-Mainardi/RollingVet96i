f8d6202af5392742ce6d283b3b539aba402af65c
Lucia Craven Garat <luciacraven93@gmail.com>
2024-08-05 12:37:51 +0200

    creación de formulario de creación de turnos en vista administrar turnos. CRUD en index.js y db.json

--- rollingVet/front/src/utils/index.js
+++ rollingVet/front/src/utils/index.js
@@ -1,4 +1,5 @@
 const urlPacientes = "http://localhost:3000/pacientes";
+const urlTurnos = "http://localhost:3000/turnos"
 import axios from 'axios';
 import { compareSync } from 'bcryptjs-react';
 
@@ -39,3 +40,56 @@ export const ingresoPaciente = async (obj) => {
         return paciente
     }
 }
+
+export const validarTurno = async (hora, fecha, sucursal) => {
+    let data = await traerTurnos()
+    let respuesta = data.find((turno) => {
+        if (turno.hora && turno.fecha && turno.sucursal !== hora, fecha, sucursal) {
+            return turno;
+        } else {
+            
+        }
+    })
+    return respuesta
+}
+
+export const traerTurnos = async()=>
+{
+    try {
+        let turnos = await axios.get(`${urlTurnos}/`);
+        console.log(turnos);
+        
+    return turnos.json()
+    } catch (error){
+console.log(error);
+
+    }
+}
+
+export const crearTurno = async(obj)=> {
+    try {
+        let turno = await axios.post(urlTurnos, obj);
+        return turno
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const actualizarTurno = async(id, obj)=>{ 
+    console.log(obj);
+    try {
+        let turno = await axios.put(`${urlTurnos}/${id}`, obj);
+        return turno
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const eliminarTurno = async(id)=> {
+    try {
+        let turno = await axios.delete(`${urlTurnos}/${id}`);
+        return turno
+    } catch (error) {
+        console.log(error);
+    }
+};
--- rollingVet/front/src/views/AdministrarTurnos.jsx
+++ rollingVet/front/src/views/AdministrarTurnos.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Form, Button } from "react-bootstrap";
+//import { validarTurno, crearTurno, traerTurnos, actualizarTurno, eliminarTurno } from "../utils/index";
+//import Swal from 'sweetalert2'
+
+
+
 
 const AdministrarTurnos = () => {
-  return (
-    <div>AdministrarTurnos</div>
-  )
-}
 
-export default AdministrarTurnos
+  const crearTurno = ({cargarData}) => {
+    const {register, handleSubmit, formState:{errors}, reset}=useForm();
+    const addItem = (obj)=> {
+      cargarData(obj);
+      reset()
+    }
+  }
+
+  // const modeloTurno = 
+  // {
+  //     "paciente": 0,
+	// 		"propietario": "",
+	// 		"telefono": "",
+	// 		"sucursal": "",
+	// 		"motivoConsulta": "",
+	// 		"fecha": "",
+	// 		"hora": "",
+	// 		"estado": true
+  //   };
+
+  
+  //   let turnosAsignados=[]
+  //   const crearTurno = async () => {
+  //   turnosAsignados = await traerTurnos();
+    
+  //   return turnosAsignados
+    
+    
+  // }
+  // crearTurno();
+  // console.log(turnosAsignados);
+  
+
+  return (
+    <>
+      <div>AdministrarTurnos</div>
+      <Form onSubmit={handleSubmit=(addItem)} method="POST">
+        <Form.Control type="date" name="fecha"  className="bg-danger" />
+        <Form.Select name="hora" >
+          <option value="">9:00</option>
+          <option value="">9:30</option>
+          <option value="">10:00</option>
+          <option value="">10:30</option>
+          <option value="">11:00</option>
+          <option value="">11:30</option>
+          <option value="">12:00</option>
+          <option value="">12:30</option>
+          <option value="">13:00</option>
+          <option value="">13:30</option>
+        </Form.Select>
+        <Form.Select>
+          <option value="">Sucursal 1</option>
+          <option value="">Sucursal 2</option>
+        </Form.Select>
+        
+
+        
+      <Form.Group className="mb-3"  controlId="input-propietario">
+        <Form.Label>Propietario</Form.Label>
+        <Form.Control type="text" placeholder="Juan Ignacio Valdez" name="propietario" />
+      </Form.Group>
+      <Form.Group  className="mb-3" controlId="input-paciente">
+        <Form.Label>Paciente</Form.Label>
+        <Form.Control type="text" placeholder="perro Labrador" name="paciente" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="input-motivo">
+        <Form.Label>Motivo</Form.Label>
+        <Form.Control  type="text" placeholder="motivo de la Consulta" name="motivoConsulta" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="input-tel">
+        <Form.Label>Teléfono</Form.Label>
+        <Form.Control  type="number" placeholder="3515695548" name="telefono" />
+      </Form.Group>
+    
+      <Button type="submit" onClick={crearTurno()}>Crear Turno</Button> 
+      </Form>
+      
+    </>
+  );
+};
+
+export default AdministrarTurnos;
